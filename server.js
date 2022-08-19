ccf19e3195c1da47bf5ce4bdd855dc4ecd7f6a2b
Kumuditha Dodanwela <86454991+KD23243@users.noreply.github.com>
2022-08-19 16:28:19 +0530

    version

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require("http");
+var _NAME;
 
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Auto deployment works! 2\n');
+   response.end(_NAME);
 }).listen(8081);
 
 console.log('Server running at http://127.0.0.1:8081/');
